2168d2f724241d28dd9001b885fd86ac9350d821
Shiva040912 <shivaarun0071@gmail.com>
2026-07-04 17:46:10 +0530

    Fix CORS for frontend deployment

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://project-management-frontend-cf7e-three.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
